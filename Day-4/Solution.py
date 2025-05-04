baeadc3a4ef53f53a8ea22f55f63fd6312260056
Kunal Waghmare <kunalwaghmare1207@gmail.com>
2025-05-05 01:51:41 +0530

    Update Solution.py

--- Day-4/Solution.py
+++ Day-4/Solution.py
@@ -1,3 +1,8 @@
+# Day 4
+# Challenge: Rename all .log files to .log.bak in a folder.
+# Focus: os, file handling
+# Example Hint: os.listdir(), os.rename()
+
 import os
 
 for file in os.listdir("."):
