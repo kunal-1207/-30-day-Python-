bec3f49b882f79b28b5e65f8340e175199d52606
Kunal Waghmare <kunalwaghmare1207@gmail.com>
2025-05-07 12:09:07 +0530

    Create Solution.py

---
+++ Day-7/Solution.py
@@ -0,0 +1,12 @@
+# Day 7
+# Challenge: Build a script that watches CPU usage and alerts if >80%.
+# Focus: psutil
+# Install with pip
+
+import psutil
+
+cpu = psutil.cpu_percent(interval=1)
+if cpu > 80:
+    print(f"ALERT! High CPU usage: {cpu}%")
+else:
+    print(f"CPU OK: {cpu}%")
