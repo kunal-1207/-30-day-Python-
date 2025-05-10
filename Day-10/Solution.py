d65ebc9ea104429d2871c412cff4107c8005cab0
Kunal Waghmare <kunalwaghmare1207@gmail.com>
2025-05-10 23:20:51 +0530

    Create Solution.py

---
+++ Day-10/Solution.py
@@ -0,0 +1,26 @@
+#  Day 10
+# Challenge: Retry Decorator
+# Focus: decorators, error handling
+# Hnt: try/except, time.sleep()
+
+import time
+import random
+
+def retry(func):
+    def wrapper(*args, **kwargs):
+        for i in range(3):
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                print(f"Retry {i+1}: {e}")
+                time.sleep(2)
+        raise Exception("Failed after 3 retries")
+    return wrapper
+
+@retry
+def flaky():
+    if random.random() < 0.7:
+        raise ValueError("Random failure")
+    return "Success"
+
+print(flaky())
